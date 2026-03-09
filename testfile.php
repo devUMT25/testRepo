37325b5f9c35dc054c6cc2c86918572272b6c30e
ali <ali@umt.com>
2026-03-09 12:54:51 +0500

    created a file

---
+++ testfile.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP File</title>
+</head>
+<body>
+    <h2>Testing PHP</h2>
+    <?php echo "Hello"; ?>
+</body>
+</html>
